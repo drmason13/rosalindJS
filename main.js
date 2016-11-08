78c2224370857729ebee9dc37f0e63285098aae8
drmason13 <drmason13@gmail.com>
2016-11-08 00:26:34 +0000

    main.js added. used for file io and individual problem solutions successfully used to solve GC problem with manual copy-paste from console

---
+++ main.js
@@ -0,0 +1,65 @@
+// "requirements"
+const analysis = require('./analysis.js');
+const fs = require("fs");
+const readline = require("readline");
+
+// testing
+var path = './io/';
+var filename = 'rosalind_gc.txt';
+var fullpath = path + filename;
+
+// setting up ready to readline
+const rl = readline.createInterface({
+    input : fs.createReadStream(fullpath, {
+        encoding : "utf8",
+    }),
+});
+
+//parse data from file using a readstream
+var currentId;
+var currentString;
+var DNAstore = [];
+
+rl.on('line', function(line){
+    //console.log();
+    //console.log(line);
+    if (line.charAt(0) == '>'){
+        //console.log('New ID found; making DNA object...');
+        try {
+            DNAstore.push(new analysis.DNA(currentString, currentId));
+            //console.log(DNAstore);
+        }
+        catch(err){
+            //console.log(err);
+            //cnosole.log("This is the first ID found, carry on...");
+        }
+        //console.log("current ID: ", line);
+        currentId = line.slice(1);
+        currentString = '';
+    }
+    else {
+        currentString += line;
+        //console.log("current string: ", currentString, " , after adding: ", line);
+    }
+});
+
+rl.on('close', function(line){
+    ////console.log('last line. Creating last DNA object...');
+    ////console.log(currentString);
+        DNAstore.push(new analysis.DNA(currentString, currentId));
+    ////console.log(DNAstore);
+    
+    // code here to wait for asynchronous call to finish!
+    
+    var answer = DNAstore.reduce((dnaX, dnaY) => {
+        if (dnaX.computeXYcontent() > dnaY.computeXYcontent()){
+            return dnaX;
+        }
+        else {
+            return dnaY;
+        }
+    });
+    console.log(answer.id);
+    console.log(answer.computeXYcontent());
+});
+
